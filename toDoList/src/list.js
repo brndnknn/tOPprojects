86f6cf173e7ce3cc205a5ca8ced92ca117600237
brndnknn <brndnknn@gmail.com>
2022-03-17 11:43:35 -0500

    added functions to list and task modules

--- toDoList/src/list.js
+++ toDoList/src/list.js
@@ -5,5 +5,33 @@ export default class List {
         this.tasks = tasks;
     }
 
+    setTitle(value){
+        this.title = value;
+    }
+
+    getTitle(){
+        return this.title;
+    }
+
+    setNotes(value){
+        this.title = value;
+    }
+
+    getNotes(){
+        return this.notes;
+    }
+
+    addTask(task){
+        this.tasks.push(task);
+    }
+
+    removeTask(taskIndex){
+            this.tasks.splice(taskIndex, 1);
+    }
+    
+    getTasks(){
+        return this.tasks;
+    }
+
     
 };
--- toDoList/src/task.js
+++ toDoList/src/task.js
@@ -1,13 +1,41 @@
 export default class Task {
     constructor( description, dueDate, priority, status ){
         this.description = description;
-        this.duDate = dueDate;
+        this.dueDate = dueDate;
         this.priority = priority;
         this.status = status;
     }
 
     setDescription(value){
         this.description = value;
+    }
+
+    getDescription(){
+        return this.description;
+    }
+
+    setDueDate(value){
+        this.dueDate = value;
+    }
+
+    getDueDate(){
+        return this.dueDate;
+    }
+
+    setPriority(value){
+        this.priority = value;
+    }
+
+    getPriority(){
+        return this.priority;
+    }
+
+    setStatus(value){
+        this.status = value;
+    }
+
+    getStatus(){
+        return this.status;
     }
 
 };
